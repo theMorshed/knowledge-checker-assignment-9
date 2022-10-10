a11e93c550803751fe0deecc62d741c3c485388e
theMorshed <themorshedctg@gmail.com>
2022-10-11 00:53:35 +0600

    create quiz page and single quiz page

--- src/components/SingleQuiz.js
+++ src/components/SingleQuiz.js
@@ -6,7 +6,20 @@ const SingleQuiz = ({ quiz }) => {
     return (
         <div>
             <p className='text-3xl font-bold'>Quiz : {question}</p>
-                     
+            <form action="">
+                {
+                    options.map(option => {
+                        return (
+                            <div className="form-control">
+                                <label className="label cursor-pointer justify-start">
+                                    <input type="radio" name="radio-6" className="radio checked:bg-blue-500" checked />
+                                    <span className="label-text ml-3">{option}</span>
+                                </label>
+                            </div>
+                        );
+                    })
+                }
+            </form>            
         </div>
     );
 };
