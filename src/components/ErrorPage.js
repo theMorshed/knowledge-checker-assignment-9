743a43e6e0aa373fbcfbaafb58dc84a6afcd4ad8
theMorshed <themorshedctg@gmail.com>
2022-10-11 09:44:18 +0600

    complete error page

--- src/components/ErrorPage.js
+++ src/components/ErrorPage.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { useRouteError } from 'react-router-dom';
+import Header from './Header';
 
 const ErrorPage = () => {
+    const error = useRouteError();
     return (
-        <div>
-            <h1>This is error page.</h1>
+        <div className='w-4/5 mx-auto'>
+            <Header />
+            <p className='text-3xl text-center mt-5 text-red-500 font-semibold'>{error.status} - {error.statusText}</p>
         </div>
     );
 };
