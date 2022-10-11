4dcc40a2f6a88358efa34ef85fb411a6085350c1
theMorshed <themorshedctg@gmail.com>
2022-10-11 21:51:56 +0600

    a small bug fix in blog page responsive issue

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Blog = () => {
     return (
-        <div className='w-3/5 mx-auto my-10'>
+        <div className='w-10/12 md:w-4/5 lg:w-3/5 mx-auto'>
             <div className='mb-5'>
                 <h2 className='text-xl font-semibold'>What is the purpose of react router?</h2>
                 <p>React Router, and dynamic, client-side routing, allows us to build a single-page web application with navigation without the page refreshing as the user navigates. React Router uses component structure to call components, which display the appropriate information. By preventing a page refresh, and using Router or Link, which is explained in more depth below, the flash of a white screen or blank page is prevented. This is one increasingly common way of having a more seamless user experience. React router also allows the user to utilize browser functionality like the back button and the refresh page while maintaining the correct view of the application.</p>
