915b925a1c33d72c59f100318a80d4359ae464fc
theMorshed <themorshedctg@gmail.com>
2022-10-11 09:35:04 +0600

    add heroicon and toastify with display correct answer after click eye icon

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -6,7 +6,7 @@ const Quiz = () => {
     const quizData = useLoaderData();
     const quizes = quizData.data.questions;
     return (
-        <div>
+        <div className='my-10 w-3/5 mx-auto'>
             {
                 quizes.map((quiz, index) => <SingleQuiz key={quiz.id} quiz={quiz} index={index} />)
             }
--- src/components/SingleQuiz.js
+++ src/components/SingleQuiz.js
@@ -1,24 +1,33 @@
 import React from 'react';
+import { EyeIcon } from '@heroicons/react/24/solid';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const SingleQuiz = ({ quiz, index }) => {
-    const { question, options } = quiz;
+    const { question, options, correctAnswer } = quiz;
+    const answer = () => toast(correctAnswer);
+    console.log(correctAnswer);
     return (
-        <div>
-            <p className='text-3xl font-bold'>Quiz {index + 1}: {question}</p>
+        <div className='mb-4'>
+            <div className='flex justify-between'>
+                <p className='text-2xl font-bold'>Quiz {index + 1}: {question}</p>
+                <EyeIcon onClick={answer} className="ml-5 w-8 h-8 text-primary cursor-pointer" />
+                <ToastContainer />
+            </div>
             <form action="">
                 {
                     options.map(option => {
                         return (
                             <div className="form-control">
                                 <label className="label cursor-pointer justify-start">
-                                    <input type="radio" name="radio-6" className="radio checked:bg-blue-500" checked />
+                                    <input type="radio" name="radio-6" className="radio checked:bg-primary" checked />
                                     <span className="label-text ml-3">{option}</span>
                                 </label>
                             </div>
                         );
                     })
                 }
-            </form>            
+            </form>
         </div>
     );
 };
